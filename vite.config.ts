bebe6ed7a8e3d945d3721e04b0e98277aec87e0a
Yusuf YILDIZ <a.yusuf.yildiz@hotmail.com>
2023-08-16 17:41:45 +0300

    change build mode for website

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/lib/index.ts'),
-      name: 'react-weekview',
-      fileName: (format) => `index.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React'
+  build:
+    mode !== "web"
+      ? {
+          lib: {
+            entry: path.resolve(__dirname, "src/lib/index.ts"),
+            name: "react-weekview",
+            fileName: (format) => `index.${format}.js`,
+          },
+          rollupOptions: {
+            external: ["react", "react-dom", "date-fns"],
+            output: {
+              globals: {
+                react: "React",
+                "date-fns": "dateFns",
+              },
+            },
+          },
         }
-      }
-    }
-  }
-})
+      : undefined,
+}));
