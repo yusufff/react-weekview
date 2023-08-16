a09cb9cf1133fae1a6318fde655640e4442a5817
Yusuf YILDIZ <a.yusuf.yildiz@hotmail.com>
2023-08-16 17:00:54 +0300

    feat: export grid component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,10 +2,12 @@ import useWeekView from "./use-weekview";
 import WeekView from "./weekview";
 import Header from "./header";
 import DaysHeader from "./days-header";
+import Grid from "./grid";
 
 export {
   useWeekView,
   WeekView,
   Header as WeekViewHeader,
   DaysHeader as WeekViewDaysHeader,
+  Grid as WeekViewGrid,
 };
