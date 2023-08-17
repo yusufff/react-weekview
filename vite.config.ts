3705ccd47f2a7bbbac1a38811a0a0eb8c0bd843e
Yusuf YILDIZ <a.yusuf.yildiz@hotmail.com>
2023-08-17 11:35:05 +0300

    generate dts files on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build:
     mode !== "web"
       ? {
